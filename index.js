a012fd7e0430fd0e1ec33d91b44391bc5909a145
gavin-cannon <gavincannon@byui.edu>
2023-10-06 17:52:15 -0600

    Added CORS code so it will work with Bro Birch's React Front End app.

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const swaggerDocument = require('./swagger.json');
 
 
 app
+    .use((req, res, next) => {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+        );
+        res.setHeader("Content-Type", "application/json");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        next();
+    })
     .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
     .use('/', require('./routes/contacts.js'));
 
