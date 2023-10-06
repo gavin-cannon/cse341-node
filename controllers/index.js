e0e4a3c4ddf88eb034f67fbc56f9539a58d6977b
gavin-cannon <gavincannon@byui.edu>
2023-10-06 17:57:05 -0600

    Added try and catch blocks so my code won't shut down my database if something goes wrong.

--- controllers/index.js
+++ controllers/index.js
@@ -19,96 +19,110 @@ const client = new MongoClient(uri, {
 });
 
 const threeContacts = (req, res, next) => {
-
-    async function run() {
-        try {
-            await client.connect();
-            const database = client.db("CSE-341");
-            const contacts = database.collection("Contacts");
-            const contactsJson = await contacts.find({}).toArray()
-            res.json(contactsJson);
-        } finally {
-            // Ensures that the client will close when you finish/error
-            await client.close();
+    try {
+        async function run() {
+            try {
+                await client.connect();
+                const database = client.db("CSE-341");
+                const contacts = database.collection("Contacts");
+                const contactsJson = await contacts.find({}).toArray()
+                res.json(contactsJson);
+            } finally {
+                // Ensures that the client will close when you finish/error
+                await client.close();
+            }
         }
+        run().catch(console.dir);
+    } catch (err) {
+        res.status(500).json(err);
     }
-    run().catch(console.dir);
-
 }
 
 const oneContact = (req, res, next) => {
-
-    async function run() {
-        try {
-            await client.connect();
-            const database = client.db("CSE-341");
-            const contacts = database.collection("Contacts");
-            const singleContactJson = await contacts.findOne({ _id: new ObjectId(req.params.userId) });
-            res.json(singleContactJson);
-        } finally {
-            // Ensures that the client will close when you finish/erro
-            await client.close();
+    try {
+        async function run() {
+            try {
+                await client.connect();
+                const database = client.db("CSE-341");
+                const contacts = database.collection("Contacts");
+                const singleContactJson = await contacts.findOne({ _id: new ObjectId(req.params.userId) });
+                res.json(singleContactJson);
+            } finally {
+                // Ensures that the client will close when you finish/erro
+                await client.close();
+            }
         }
+        run().catch(console.dir);
+    } catch (err) {
+        res.status(500).json(err);
     }
-    run().catch(console.dir);
-
 }
 
 const createContact = (req, res, next) => {
+    try {
+        async function run() {
 
-    async function run() {
+            try {
+                await client.connect();
+                const database = client.db("CSE-341");
+                const contacts = database.collection("Contacts");
+                const newContact = req.body;
+                const result = await contacts.insertOne(newContact);
+                console.log(result);
+                res.status(201).json(result);
 
-        try {
-            await client.connect();
-            const database = client.db("CSE-341");
-            const contacts = database.collection("Contacts");
-            const newContact = req.body;
-            const result = await contacts.insertOne(newContact);
-            console.log(result);
-            res.status(201).json(result);
+            } finally {
+                await client.close();
+            }
 
-        } finally {
-            await client.close();
         }
 
+        run().catch(console.dir);
+    } catch (err) {
+        res.status(500).json(err);
     }
-
-    run().catch(console.dir);
 }
 
 const updateContact = (req, res, next) => {
+    try {
+        async function run() {
 
-    async function run() {
-
-        try {
-            await client.connect();
-            const database = client.db("CSE-341");
-            const contacts = database.collection("Contacts");
-            const result = await contacts.updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body });
-            res.status(204).send("No Content");
-        } finally {
-            await client.close();
+            try {
+                await client.connect();
+                const database = client.db("CSE-341");
+                const contacts = database.collection("Contacts");
+                const result = await contacts.updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body });
+                res.status(204).send("No Content");
+            } finally {
+                await client.close();
+            }
         }
-    }
 
-    run().catch(console.dir);
+        run().catch(console.dir);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 }
 const deleteContact = (req, res, next) => {
-    console.log(req.params.id);
-    async function run() {
+    try {
 
-        try {
-            await client.connect();
-            const database = client.db("CSE-341");
-            const contacts = database.collection("Contacts");
-            const result = await contacts.deleteOne({ _id: new ObjectId(req.params.id) });
-            console.log(result);
-            res.status(200).send("OK");
-        } finally {
-            await client.close();
+        async function run() {
+
+            try {
+                await client.connect();
+                const database = client.db("CSE-341");
+                const contacts = database.collection("Contacts");
+                const result = await contacts.deleteOne({ _id: new ObjectId(req.params.id) });
+                console.log(result);
+                res.status(200).send("OK");
+            } finally {
+                await client.close();
+            }
         }
+        run().catch(console.dir);
+    } catch (err) {
+        res.status(500).json(err);
     }
-    run().catch(console.dir);
 }
 
 module.exports = { bethJson, threeContacts, oneContact, createContact, updateContact, deleteContact };
