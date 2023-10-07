19ecc8f9cbe802f1657f1e228bc0dfdd4a132cbc
gavin-cannon <gavincannon@byui.edu>
2023-10-06 22:03:27 -0600

    Revert autobody test

--- controllers/index.js
+++ controllers/index.js
@@ -59,11 +59,7 @@ const oneContact = (req, res, next) => {
 }
 
 const createContact = (req, res, next) => {
-    /*  #swagger.parameters['obj'] = {
-        in: 'body',
-        description: 'Add a user',
-        schema: { asdf: 'qwer' }
-    } */
+
     try {
         async function run() {
 
