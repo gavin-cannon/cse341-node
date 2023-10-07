643061d617f042fcca45e39b77c17825a75c9e68
gavin-cannon <gavincannon@byui.edu>
2023-10-06 20:37:22 -0600

    logged swagger doc

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
-
+console.log(swaggerDocument);
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
