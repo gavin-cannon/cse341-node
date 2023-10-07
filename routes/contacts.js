b25f0ec02ad55a6d091aa173fc3feef630ba4714
gavin-cannon <gavincannon@byui.edu>
2023-10-06 21:39:40 -0600

    added post comment parameters

--- routes/contacts.js
+++ routes/contacts.js
@@ -9,6 +9,11 @@ routes.use("/", require("./swagger"));
 routes.get('/contacts', controller.threeContacts);
 routes.get('/contacts/:userId', controller.oneContact);
 routes.post('/contacts', bodyParser.json(), controller.createContact);
+/*  #swagger.parameters['obj'] = {
+                in: 'body',
+                description: 'Add a user',
+                schema: { $ref: '#/definitions/AddUser' }
+        } */
 routes.put('/contacts/:id', bodyParser.json(), controller.updateContact);
 routes.delete('/contacts/:id', bodyParser.json(), controller.deleteContact);
 
