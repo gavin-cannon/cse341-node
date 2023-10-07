296299f049e9a0e38eb8617110242160a58b233e
gavin-cannon <gavincannon@byui.edu>
2023-10-06 19:39:22 -0600

    changes routes to fit in with react front end

--- routes/contacts.js
+++ routes/contacts.js
@@ -8,8 +8,8 @@ const bodyParser = require("body-parser");
 routes.use("/", require("./swagger"));
 routes.get('/contacts', controller.threeContacts);
 routes.get('/contacts/:userId', controller.oneContact);
-routes.post('/', bodyParser.json(), controller.createContact);
-routes.put('/:id', bodyParser.json(), controller.updateContact);
-routes.delete('/:id', bodyParser.json(), controller.deleteContact);
+routes.post('/contacts', bodyParser.json(), controller.createContact);
+routes.put('/contacts/:id', bodyParser.json(), controller.updateContact);
+routes.delete('/contacts/:id', bodyParser.json(), controller.deleteContact);
 
 module.exports = routes;
