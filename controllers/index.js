17a4d3f6e7af63164db88b79515812d37c37e5a1
gavin-cannon <gavincannon@byui.edu>
2023-10-07 19:47:13 -0600

    added schema and updated swagger json

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,8 @@
 const bethJson = (req, res, next) => {
-    res.json(`Beth O'Driscoll`)
-}
+  res.json(`Beth O'Driscoll`);
+};
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const dotenv = require("dotenv").config({ path: ".env" });
 
@@ -11,125 +10,161 @@ const uri = process.env.CONNECTION_STRING;
 
 const bodyParser = require("body-parser");
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        decprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    decprecationErrors: true,
+  },
 });
 
 const threeContacts = (req, res, next) => {
-    try {
-        async function run() {
-            try {
-                await client.connect();
-                const database = client.db("CSE-341");
-                const contacts = database.collection("Contacts");
-                const contactsJson = await contacts.find({}).toArray()
-                res.json(contactsJson);
-            } finally {
-                // Ensures that the client will close when you finish/error
-                await client.close();
-            }
-        }
-        run().catch(console.dir);
-    } catch (err) {
-        res.status(500).json(err);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  try {
+    async function run() {
+      try {
+        await client.connect();
+        const database = client.db("CSE-341");
+        const contacts = database.collection("Contacts");
+        const contactsJson = await contacts.find({}).toArray();
+        res.json(contactsJson);
+      } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+      }
     }
-}
+    run().catch(console.dir);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 
 const oneContact = (req, res, next) => {
-    try {
-        async function run() {
-            try {
-                await client.connect();
-                const database = client.db("CSE-341");
-                const contacts = database.collection("Contacts");
-                const singleContactJson = await contacts.findOne({ _id: new ObjectId(req.params.userId) });
-                res.json(singleContactJson);
-            } finally {
-                // Ensures that the client will close when you finish/erro
-                await client.close();
-            }
-        }
-        run().catch(console.dir);
-    } catch (err) {
-        res.status(500).json(err);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  try {
+    async function run() {
+      try {
+        await client.connect();
+        const database = client.db("CSE-341");
+        const contacts = database.collection("Contacts");
+        const singleContactJson = await contacts.findOne({
+          _id: new ObjectId(req.params.userId),
+        });
+        res.json(singleContactJson);
+      } finally {
+        // Ensures that the client will close when you finish/erro
+        await client.close();
+      }
     }
-}
+    run().catch(console.dir);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 
 const createContact = (req, res, next) => {
-    const contact = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        favoriteColor: req.body.favoriteColor,
-        birthday: req.body.birthday
-    };
-    try {
-        async function run() {
-
-            try {
-                await client.connect();
-                const database = client.db("CSE-341");
-                const contacts = database.collection("Contacts");
-                const newContact = req.body;
-                const result = await contacts.insertOne(newContact);
-                console.log(result);
-                res.status(201).json(result);
-
-            } finally {
-                await client.close();
-            }
-
-        }
-
-        run().catch(console.dir);
-    } catch (err) {
-        res.status(500).json(err);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  try {
+    async function run() {
+      try {
+        await client.connect();
+        const database = client.db("CSE-341");
+        const contacts = database.collection("Contacts");
+        const newContact = req.body;
+        const result = await contacts.insertOne(newContact);
+        console.log(result);
+        res.status(201).json(result);
+      } finally {
+        await client.close();
+      }
     }
-}
+
+    run().catch(console.dir);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 
 const updateContact = (req, res, next) => {
-    try {
-        async function run() {
-
-            try {
-                await client.connect();
-                const database = client.db("CSE-341");
-                const contacts = database.collection("Contacts");
-                const result = await contacts.updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body });
-                res.status(204).send("No Content");
-            } finally {
-                await client.close();
-            }
-        }
-
-        run().catch(console.dir);
-    } catch (err) {
-        res.status(500).json(err);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  try {
+    async function run() {
+      try {
+        await client.connect();
+        const database = client.db("CSE-341");
+        const contacts = database.collection("Contacts");
+        const result = await contacts.updateOne(
+          { _id: new ObjectId(req.params.id) },
+          { $set: req.body }
+        );
+        res.status(204).send("No Content");
+      } finally {
+        await client.close();
+      }
     }
-}
+
+    run().catch(console.dir);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 const deleteContact = (req, res, next) => {
-    try {
-
-        async function run() {
-
-            try {
-                await client.connect();
-                const database = client.db("CSE-341");
-                const contacts = database.collection("Contacts");
-                const result = await contacts.deleteOne({ _id: new ObjectId(req.params.id) });
-                console.log(result);
-                res.status(200).send("OK");
-            } finally {
-                await client.close();
-            }
-        }
-        run().catch(console.dir);
-    } catch (err) {
-        res.status(500).json(err);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  try {
+    async function run() {
+      try {
+        await client.connect();
+        const database = client.db("CSE-341");
+        const contacts = database.collection("Contacts");
+        const result = await contacts.deleteOne({
+          _id: new ObjectId(req.params.id),
+        });
+        console.log(result);
+        res.status(200).send("OK");
+      } finally {
+        await client.close();
+      }
     }
-}
+    run().catch(console.dir);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 
-module.exports = { bethJson, threeContacts, oneContact, createContact, updateContact, deleteContact };
+module.exports = {
+  bethJson,
+  threeContacts,
+  oneContact,
+  createContact,
+  updateContact,
+  deleteContact,
+};
