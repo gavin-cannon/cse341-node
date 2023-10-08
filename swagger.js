d91999f1aa7dcd4f436a44b385b1ff466159e9b0
gavin-cannon <gavincannon@byui.edu>
2023-10-07 19:07:53 -0600

    changed port again.

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: "My API",
     description: "Description",
   },
-  host: "localhost:5000",
-  schemes: ["http"],
+  host: "cse341-node-56ek.onrender.com",
+  schemes: ["https"],
 };
 
 const outputFile = "./swagger.json";
